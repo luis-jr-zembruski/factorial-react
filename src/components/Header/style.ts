852506dddbf4038219031f0228627dffcdb37707
luis-jr-zembruski <luis.jr.zembruski@icloud.com>
2021-10-13 15:19:21 -0300

    inicio da estilização

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,5 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+    width: 100vw;
+    height: 15vh;
 
+    background: #aaa;
+    
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    
 `
